e9b3a450919fc774af26105dfd6b753610a7d96a
Echo5414 <sschiener@outlook.de>
2024-12-30 19:31:57 +0100

    Update for Production

--- config/server.ts
+++ config/server.ts
@@ -4,7 +4,9 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('BACKEND_URL', 'http://localhost:1337'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  prefix: env('NODE_ENV') === 'production' ? '' : '/api',
 });
